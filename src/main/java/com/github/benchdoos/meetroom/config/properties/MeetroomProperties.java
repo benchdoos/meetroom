9c3c89a48afeb9655ca56734d9c90ca02fa1e1db
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-01-30 20:21:56 +0300

    Added meeting room properties

---
+++ src/main/java/com/github/benchdoos/meetroom/config/properties/MeetroomProperties.java
@@ -0,0 +1,38 @@
+package com.github.benchdoos.meetroom.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Properties for meeting rooms
+ */
+@Getter
+@Setter
+@Validated
+@Component
+@ConfigurationProperties("meetroom")
+//todo here we can add custom annotation to validate that minimumReservationValue is >= maximumReservationValue using reflection
+public class MeetroomProperties {
+    /**
+     * Minimum meet room reservation value in minutes
+     */
+    @NotNull
+    @Min(value = 1)
+    @Max(value = 1440)
+    private Integer minimumReservationValue;
+
+    /**
+     * Maximum meet room reservation value in minutes
+     */
+    @NotNull
+    @Min(value = 1)
+    @Max(value = 1440)
+    private Integer maximumReservationValue;
+}
