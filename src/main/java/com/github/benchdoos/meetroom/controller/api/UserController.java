c334ae5a699fec5a939c6fa74b473bd5a493d6a3
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-02-03 13:45:07 +0300

    Updated url

--- src/main/java/com/github/benchdoos/meetroom/controller/api/UserController.java
+++ src/main/java/com/github/benchdoos/meetroom/controller/api/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
     public final UserService userService;
 
