298d23a99344e0c7a7569a10d0cb80346e81a915
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-02-04 23:07:54 +0300

    Updated ui Fixed dependencies with User - UserRole

--- src/main/java/com/github/benchdoos/meetroom/domain/User.java
+++ src/main/java/com/github/benchdoos/meetroom/domain/User.java
@@ -10,11 +10,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.Set;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
@@ -28,24 +29,24 @@ public class User {
     @GeneratedValue
     private UUID id;
 
-    @NotNull
+    @NotBlank
     @Column(unique = true)
     private String username;
 
-    @NotNull
+    @NotBlank
     private String password;
 
-    @NotNull
+    @NotBlank
     private String firstName;
 
-    @NotNull
+    @NotBlank
     private String lastName;
 
     @NotNull
     private boolean enabled;
 
     @NotEmpty
-    @OneToMany(cascade = CascadeType.ALL)
-    private Set<UserRole> roles;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Collection<UserRole> roles;
 
 }
--- src/main/java/com/github/benchdoos/meetroom/domain/UserRole.java
+++ src/main/java/com/github/benchdoos/meetroom/domain/UserRole.java
@@ -5,11 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Entity
@@ -23,6 +24,11 @@ public class UserRole {
     @GeneratedValue
     UUID id;
 
-    @NotNull
+    @NotBlank
+    @Column(unique = true)
     String role;
+
+    @NotBlank
+    @Column(unique = true)
+    String name;
 }
