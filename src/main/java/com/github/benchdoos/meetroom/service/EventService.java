e5d0e55c83256902601af2f073805037c4e0f90b
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-02-08 22:34:32 +0300

    Added JavaDoc

--- src/main/java/com/github/benchdoos/meetroom/service/EventService.java
+++ src/main/java/com/github/benchdoos/meetroom/service/EventService.java
@@ -11,13 +11,42 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Service that gives ability to operate with {@link Event}
+ */
 public interface EventService {
 
+    /**
+     * Get list of events by meeting room and date range.
+     *
+     * @param meetingRoom meeting room to look by
+     * @param fromDate start of date range
+     * @param toDate end of date range
+     * @return only not marked as deleted values
+     */
     List<Event> getEvents(MeetingRoom meetingRoom, ZonedDateTime fromDate, ZonedDateTime toDate);
 
+    /**
+     * Get list of events by meeting room.
+     * @param meetingRoom meeting room to look by
+     * @param pageable pageable
+     * @return all meeting room events (even marked as deleted).
+     */
     Page<Event> getAllEvents(MeetingRoom meetingRoom, Pageable pageable);
 
+    /**
+     * Get event info by id
+     *
+     * @param id of event
+     * @return info about event
+     */
     EventDto getEventDtoById(UUID id);
 
+    /**
+     * Create event
+     *
+     * @param createEventDto dto
+     * @return created event
+     */
     Event createEvent(CreateEventDto createEventDto);
 }
