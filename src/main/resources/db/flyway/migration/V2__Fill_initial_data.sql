4e29b4c04c0d865bae2c3017289b04a5141476d5
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-02-09 20:47:22 +0300

    Updated script

--- src/main/resources/db/flyway/migration/V2__Fill_initial_data.sql
+++ src/main/resources/db/flyway/migration/V2__Fill_initial_data.sql
@@ -21,3 +21,9 @@ INSERT INTO public.users_roles (user_id, roles_id)
 VALUES ('a1b43961-b306-400b-82cc-b7d933c9828d', '2936d396-9fe0-48a2-b07c-24c0b5f031df');
 INSERT INTO public.users_roles (user_id, roles_id)
 VALUES ('fda7f710-7429-456a-a0d6-d1205c7e94cd', 'f0d09ed9-e672-457c-ac3e-f5ccfc62f228');
+
+--Creating event rooms
+INSERT INTO public.meeting_rooms (id, location, name, enabled)
+VALUES ('5ce162b2-4aa1-43a7-a45d-c3be0664f5e5', 'Block A. Room 9.', 'Main meeting room', true);
+INSERT INTO public.meeting_rooms (id, location, name, enabled)
+VALUES ('068e7b3f-cca6-45ee-8429-8a91b6e90f1c', 'Block A. Room 10.', 'Secondary meeting room', true);
