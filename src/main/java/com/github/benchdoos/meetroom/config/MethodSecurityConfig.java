52e711f2a085cd3b0fe25127643345b7450f9fb7
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-03 20:27:33 +0300

    Code cleanup

--- src/main/java/com/github/benchdoos/meetroom/config/MethodSecurityConfig.java
+++ src/main/java/com/github/benchdoos/meetroom/config/MethodSecurityConfig.java
@@ -8,6 +8,6 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 @EnableGlobalMethodSecurity(
         prePostEnabled = true,
         securedEnabled = true)
-public class MethodSecurityConfig
-        extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
 }
