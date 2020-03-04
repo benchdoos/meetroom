236b979d366147a89f9c91fd8fbd2743361184c1
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-04 21:46:08 +0300

    Added test for DateRangeDuration

--- src/test/java/com/github/benchdoos/meetroom/utils/DateUtilsTest.java
+++ src/test/java/com/github/benchdoos/meetroom/utils/DateUtilsTest.java
@@ -64,6 +64,34 @@ class DateUtilsTest extends AbstractUnitTest {
         assertThat(weekRange.getToDate()).isEqualTo(DateUtils.truncateTimeToDayEnd(weekRange.getToDate()));
     }
 
+    /**
+     * @see DateUtils#getDateRangeDuration(ZonedDateTime, ZonedDateTime) why 1441 instead of 1440
+     */
+    @Test
+    void getDateRangeDurationForDay() {
+        final ZonedDateTime fromDate = easyRandom.nextObject(ZonedDateTime.class);
+        final ZonedDateTime toDate = fromDate.plusDays(1);
+
+        final long dateRangeDuration = DateUtils.getDateRangeDuration(fromDate, toDate);
+
+        final int hoursInDay = 24 * 60;
+        assertThat(dateRangeDuration).isEqualTo(hoursInDay + 1);
+    }
+
+    /**
+     * @see DateUtils#getDateRangeDuration(ZonedDateTime, ZonedDateTime) why ‭10081‬ instead of ‭10080‬
+     */
+    @Test
+    void getDateRangeDurationForWeek() {
+        final ZonedDateTime fromDate = easyRandom.nextObject(ZonedDateTime.class);
+        final ZonedDateTime toDate = fromDate.plusWeeks(1);
+
+        final long dateRangeDuration = DateUtils.getDateRangeDuration(fromDate, toDate);
+
+        final int hoursInWeek = 7 * 24 * 60;
+        assertThat(dateRangeDuration).isEqualTo(hoursInWeek + 1);
+    }
+
     private void assertDateDayEquals(ZonedDateTime now, ZonedDateTime truncateNow) {
         assertThat(truncateNow).isNotNull();
         assertThat(truncateNow.getYear()).isEqualTo(now.getYear());
