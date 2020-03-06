6e4f19dc8bb79c3c8f8dd271ffb14fff821d2cff
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-03-06 17:20:07 +0300

    meetroom-2: Hotfix of migration

--- src/main/resources/db/flyway/migration/V7__Added_roles_names.sql
+++ src/main/resources/db/flyway/migration/V7__Added_roles_names.sql
@@ -1,7 +1,7 @@
-update meetroom.public.roles
+update public.roles
 set name = 'Admin'
 where id = '2936d396-9fe0-48a2-b07c-24c0b5f031df';
 
-update meetroom.public.roles
+update public.roles
 set name = 'User'
 where id = 'f0d09ed9-e672-457c-ac3e-f5ccfc62f228';
