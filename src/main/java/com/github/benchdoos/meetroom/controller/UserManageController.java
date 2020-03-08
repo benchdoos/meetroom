ff945efeb0cf28959805619610f2d95165f9e38c
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-08 21:27:46 +0300

    meetroom-7: Added User role modal

--- src/main/java/com/github/benchdoos/meetroom/controller/UserManageController.java
+++ src/main/java/com/github/benchdoos/meetroom/controller/UserManageController.java
@@ -1,16 +1,21 @@
 package com.github.benchdoos.meetroom.controller;
 
 import com.github.benchdoos.meetroom.domain.User;
+import com.github.benchdoos.meetroom.domain.UserRole;
+import com.github.benchdoos.meetroom.service.UserRoleService;
 import com.github.benchdoos.meetroom.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,11 +24,16 @@ public class UserManageController {
 
     private final UserService userService;
 
+    private final UserRoleService userRoleService;
+
     @PreAuthorize("hasAnyAuthority('MANAGE_USERS:USE')")
     @GetMapping
     public String getMainPage(@PageableDefault Pageable pageable, Model model) {
         final Page<User> allUsers = userService.getAllUsers(pageable);
+        final List<UserRole> allUserRoles = userRoleService.getAllUserRoles(Sort.by(Sort.Order.asc("name")));
+
         model.addAttribute("users", allUsers);
+        model.addAttribute("roles", allUserRoles);
         return "manage/users";
     }
 
