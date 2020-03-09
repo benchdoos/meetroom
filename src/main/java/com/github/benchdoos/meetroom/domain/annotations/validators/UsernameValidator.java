e4e1f223b96ee12f4d80c8b1f293950ea102099c
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-09 17:37:10 +0300

    meetroom-7: Fixed validator

--- src/main/java/com/github/benchdoos/meetroom/domain/annotations/validators/UsernameValidator.java
+++ src/main/java/com/github/benchdoos/meetroom/domain/annotations/validators/UsernameValidator.java
@@ -1,7 +1,6 @@
 package com.github.benchdoos.meetroom.domain.annotations.validators;
 
 import com.github.benchdoos.meetroom.domain.annotations.Username;
-import com.google.common.base.CharMatcher;
 import org.springframework.util.StringUtils;
 
 import javax.validation.ConstraintValidator;
@@ -14,7 +13,6 @@ public class UsernameValidator implements ConstraintValidator<Username, String> 
 
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return StringUtils.hasText(value)
-                && !value.contains(" ")
-                && CharMatcher.javaLowerCase().matchesAllOf(value);
+                && !value.contains(" ");
     }
 }
