fefa2e7b5109785e9a476bbc2281d28a1821bb71
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-09 18:53:16 +0300

    meetroom-7: Added privilege

---
+++ src/main/resources/db/flyway/migration/V8__Added_privilege_to_manage_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.privileges (id, name)
+VALUES ('85514763-0e62-4d0a-b15d-482b219e1524', 'MANAGE_USERS:USE');
+
+INSERT INTO public.roles_privileges (roles_id, privilege_id)
+VALUES ('2936d396-9fe0-48a2-b07c-24c0b5f031df', '85514763-0e62-4d0a-b15d-482b219e1524');
