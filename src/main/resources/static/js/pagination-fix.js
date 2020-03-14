c004d5d904b8912fdebce20be8b262b541c38343
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-03-14 11:00:16 +0300

    meetroom-25: Added documentation

--- src/main/resources/static/js/pagination-fix.js
+++ src/main/resources/static/js/pagination-fix.js
@@ -1,3 +1,6 @@
+/**
+ * Gets pageable control elements and check if any of them need `page-link` class
+ */
 function fixPageLink() {
     const pageLinkClassName = "page-link";
 
@@ -16,10 +19,20 @@ function fixPageLink() {
     }
 }
 
+/**
+ * Checks if element has given class
+ *
+ * @param element element
+ * @param className class name
+ * @returns {boolean} true if element has given class
+ */
 function hasClass(element, className) {
     return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
 }
 
+/**
+ * Changes pagination control icons to FontAwesome
+ */
 function fixPaginationControlIcons() {
     //maybe not the best solution, but I'm not a front-ender so...
     $(".page-item:first-child .page-link span").html("<i class=\"fas fa-angle-double-left\"></i>");
@@ -28,6 +41,9 @@ function fixPaginationControlIcons() {
     $(".page-item:nth-last-child(2) .page-link span").html("<i class=\"fas fa-angle-right\"></i>");
 }
 
+/**
+ * Runs fixes for pageable control
+ */
 $(document).ready(function () {
     fixPageLink();
     fixPaginationControlIcons();
