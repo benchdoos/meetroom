0df5b48ccc9486935cbf86f3e185ca7c8cb40132
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-04-12 13:33:59 +0300

    meetroom-10: Added JavaDoc

--- src/main/java/com/github/benchdoos/meetroom/controller/api/v1/ApiV1UserController.java
+++ src/main/java/com/github/benchdoos/meetroom/controller/api/v1/ApiV1UserController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
+/**
+ * Rest user controller implementation.
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(ApiConstants.API_V1_PATH_PREFIX + "/user")
@@ -23,24 +26,42 @@ public class ApiV1UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
+    /**
+     * Get extended info about user by id
+     *
+     * @param id user id
+     * @return info
+     */
     @PreAuthorize("hasAnyAuthority('MANAGE_USERS:USE')")
     @GetMapping("/{id}/full")
     public UserExtendedInfoDto getUserExtendedInfoById(@PathVariable("id") UUID id) {
         return userService.getUserExtendedInfoById(id);
     }
 
+    /**
+     * Get public user info by user id
+     *
+     * @param id user id
+     * @return info
+     */
     @GetMapping("/{id}")
     public UserPublicInfoDto getUserInfoById(@PathVariable("id") UUID id) {
         @NotNull final String username = userService.getUserExtendedInfoById(id).getUsername();
         return userService.getUserPublicInfoDtoByUsername(username);
     }
 
+    /**
+     * Get extended user information by user token
+     *
+     * @param token user token
+     * @return extended user info
+     */
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/me")
-    public UserExtendedInfoDto getMe(UsernamePasswordAuthenticationToken principal) {
+    public UserExtendedInfoDto getMe(UsernamePasswordAuthenticationToken token) {
         final UserExtendedInfoDto userExtendedInfoDto = new UserExtendedInfoDto();
 
-        userMapper.convert(principal, userExtendedInfoDto);
+        userMapper.convert(token, userExtendedInfoDto);
 
         return userExtendedInfoDto;
     }
