4f67ffaf60e752aea2e2f02af37ab07217ec5045
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-04-12 12:33:31 +0300

    meetroom-10: Removed TokenStore

--- src/main/java/com/github/benchdoos/meetroom/config/security/MethodSecurityConfig.java
+++ src/main/java/com/github/benchdoos/meetroom/config/security/MethodSecurityConfig.java
@@ -5,19 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableGlobalMethodSecurity(
         prePostEnabled = true,
         securedEnabled = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
-
-    @Bean
-    public TokenStore getTokenStore() {
-        return new InMemoryTokenStore();
-    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
