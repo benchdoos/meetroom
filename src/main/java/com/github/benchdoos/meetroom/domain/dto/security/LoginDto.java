91438208d3a303ac6fb02b53c325e77dd6ec383d
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-04-12 13:35:02 +0300

    meetroom-10: Added JavaDoc

--- src/main/java/com/github/benchdoos/meetroom/domain/dto/security/LoginDto.java
+++ src/main/java/com/github/benchdoos/meetroom/domain/dto/security/LoginDto.java
@@ -5,13 +5,22 @@ import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * Dto to provide authorization for user
+ */
 @RequiredArgsConstructor
 @Getter
 public class LoginDto {
 
+    /**
+     * Username of user
+     */
     @NotNull
     private final String username;
 
+    /**
+     * Password of user
+     */
     @NotNull
     private final String password;
 }
