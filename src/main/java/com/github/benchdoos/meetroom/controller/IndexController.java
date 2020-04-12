a57d3812f3abe7abfece55279b5ad368654845e2
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-04-12 17:11:54 +0300

    meetroom-62: Moved endponit Fixed edit redirection

--- src/main/java/com/github/benchdoos/meetroom/controller/IndexController.java
+++ src/main/java/com/github/benchdoos/meetroom/controller/IndexController.java
@@ -6,19 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Date;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
@@ -36,22 +28,6 @@ public class IndexController {
     @GetMapping("/all")
     public String getAllRooms(@PageableDefault(sort = "name", direction = Sort.Direction.ASC, size = 9) Pageable pageable, Model model) {
         return modelViewService.getAllRooms(pageable, model);
-    }
-
-    @PreAuthorize("hasAnyAuthority('MEETING_ROOM:USE')")
-    @GetMapping("/{uuid}")
-    public String getMeetingRoomById(@PathVariable UUID uuid,
-
-                                     @RequestParam(value = "day", required = false)
-                                     @DateTimeFormat(pattern = "dd.MM.yyyy") Date day,
-
-                                     Model model) {
-        if (day != null) {
-            final ZonedDateTime fromDate = ZonedDateTime.ofInstant(day.toInstant(), ZoneId.systemDefault());
-            return modelViewService.getMeetingRoomById(uuid, fromDate, model);
-        }
-
-        return modelViewService.getMeetingRoomById(uuid, null, model);
     }
 
     /**
