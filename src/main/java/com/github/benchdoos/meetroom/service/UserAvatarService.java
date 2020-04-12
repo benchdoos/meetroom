4b14533361a088245a7eecbb092abc9a0b3b5f65
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-04-12 18:53:32 +0300

    meetroom-65: Added avatar service

---
+++ src/main/java/com/github/benchdoos/meetroom/service/UserAvatarService.java
@@ -0,0 +1,24 @@
+package com.github.benchdoos.meetroom.service;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public interface UserAvatarService {
+
+    /**
+     * Generates random avatar
+     *
+     * @param size size of picture
+     * @return base64 avatar image
+     */
+    String generateRandomAvatar(@Size(min = 40, max = 256) int size);
+
+    /**
+     * Generates avatar by given key-string
+     *
+     * @param key string
+     * @param size size of picture
+     * @return base64 avatar image
+     */
+    String generateAvatarForString(@NotNull String key, @Size(min = 40, max = 256) int size);
+}
