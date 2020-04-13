c153c7e220f3b9d7df1ed110d598643c54977b90
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-04-13 17:29:41 +0300

    meetroom-41: Added Ajax requests, but not totally working

---
+++ src/main/resources/static/js/requests/user/update-user-requests.js
@@ -0,0 +1,48 @@
+/**
+ * Provides api prefix to any ajax request
+ */
+$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+    let context = "/meetroom"; //fixme change to context
+    const API_PREFIX = "/api/v1";
+    options.url = context + API_PREFIX + options.url
+});
+
+
+function getUserInfo(userId) {
+    let url = "/user/" + userId;
+
+    console.log("URL: ", url);
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (output, status, xhr) {
+            console.log("Response is:", output);
+            console.log("Status is:", status);
+            console.log("xhr is:", xhr);
+        }
+    });
+
+}
+
+function updateUserUsername(changeUsernameDto) {
+    let url = "/user/change-username";
+
+    console.log("URL: ", url);
+    console.log("DTO:", changeUsernameDto);
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify(changeUsernameDto),
+        success: function (output, status, xhr) {
+            console.log("Response is:", output);
+            console.log("Status is:", status);
+            console.log("xhr is:", xhr);
+        },
+        failure: function (error) {
+            console.log("error: ", error);
+        }
+    });
+}
