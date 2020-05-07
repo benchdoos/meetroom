17b504c9d97c4ca0f99164a82b54879d59b968f1
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-05-07 17:05:58 +0300

    meetroom-41: Added JavaDoc

--- src/main/java/com/github/benchdoos/meetroom/domain/Avatar.java
+++ src/main/java/com/github/benchdoos/meetroom/domain/Avatar.java
@@ -15,6 +15,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.UUID;
 
+/**
+ * Entity that stores user's avatar
+ */
 @Entity
 @Table(name = "users_avatars")
 @AllArgsConstructor
@@ -27,10 +30,17 @@ public class Avatar {
     @Column(name = "id")
     private UUID id;
 
+    /**
+     * Type of stored data.
+     */
     @Enumerated(EnumType.STRING)
     @Column(name = "type",length = 36)
     private AvatarDataType type;
 
+    /**
+     * Avatar data. In case of {@link AvatarDataType#BASE64} it can be base64 string.
+     * In case of {@link AvatarDataType#GRAVATAR} can be email address.
+     */
     @Column(name = "data", length = 98304)
     private String data;
 }
