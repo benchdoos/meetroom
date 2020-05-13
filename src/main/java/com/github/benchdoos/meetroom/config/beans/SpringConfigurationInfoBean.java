ee11635e9ee253f95152d248d92c0a2fe09d70bb
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-05-13 15:02:32 +0300

    Addedd test. Need to be seen on production server

--- src/main/java/com/github/benchdoos/meetroom/config/beans/SpringConfigurationInfoBean.java
+++ src/main/java/com/github/benchdoos/meetroom/config/beans/SpringConfigurationInfoBean.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import java.net.InetAddress;
 
 /**
  * Component to get {@link ServletContext} from spring bean processor
@@ -16,4 +19,33 @@ import javax.servlet.ServletContext;
 @Component
 public class SpringConfigurationInfoBean {
     private final ServletContext servletContext;
+    private final ServletRequest servletRequest;
+
+    @PostConstruct
+    private void afterCreate() {
+        System.out.println("HELLO FROM POST CONSTRACT!");
+        System.out.println("------------------------------");
+        System.out.println("canonicalHostName: " + InetAddress.getLoopbackAddress().getCanonicalHostName());
+        System.out.println("hostName: " + InetAddress.getLoopbackAddress().getHostName());
+        System.out.println("hostAddress: " + InetAddress.getLoopbackAddress().getHostAddress());
+        System.out.println("------------------------------");
+        System.out.println("Secure: " + servletRequest.isSecure());
+        System.out.println("localPort: " + servletRequest.getLocalPort());
+        System.out.println("contextPath: " + servletContext.getContextPath());
+        System.out.println("------------------------------");
+        System.out.println("getPublicFullApplicationUrl: " + getPublicFullApplicationUrl());
+        System.out.println("------------------------------");
+    }
+
+    public String getPublicFullApplicationUrl() {
+        final String prefix = servletRequest.isSecure() ? "https://" : "http://";
+
+        final String fullApplicationPath = prefix
+                + InetAddress.getLoopbackAddress().getHostAddress()
+                + ":"
+                + servletRequest.getLocalPort()
+                + servletContext.getContextPath();
+        log.debug("Full application path: {}", fullApplicationPath);
+        return fullApplicationPath;
+    }
 }
