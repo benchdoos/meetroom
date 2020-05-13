b9d9ad4559834466072a6a986010241b686acdee
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-05-13 15:29:33 +0300

    Revert "Addedd test. Need to be seen on production server"
    This reverts commit ee11635e

--- src/main/java/com/github/benchdoos/meetroom/config/beans/SpringConfigurationInfoBean.java
+++ src/main/java/com/github/benchdoos/meetroom/config/beans/SpringConfigurationInfoBean.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import java.net.InetAddress;
 
 /**
  * Component to get {@link ServletContext} from spring bean processor
@@ -19,33 +16,4 @@ import java.net.InetAddress;
 @Component
 public class SpringConfigurationInfoBean {
     private final ServletContext servletContext;
-    private final ServletRequest servletRequest;
-
-    @PostConstruct
-    private void afterCreate() {
-        System.out.println("HELLO FROM POST CONSTRACT!");
-        System.out.println("------------------------------");
-        System.out.println("canonicalHostName: " + InetAddress.getLoopbackAddress().getCanonicalHostName());
-        System.out.println("hostName: " + InetAddress.getLoopbackAddress().getHostName());
-        System.out.println("hostAddress: " + InetAddress.getLoopbackAddress().getHostAddress());
-        System.out.println("------------------------------");
-        System.out.println("Secure: " + servletRequest.isSecure());
-        System.out.println("localPort: " + servletRequest.getLocalPort());
-        System.out.println("contextPath: " + servletContext.getContextPath());
-        System.out.println("------------------------------");
-        System.out.println("getPublicFullApplicationUrl: " + getPublicFullApplicationUrl());
-        System.out.println("------------------------------");
-    }
-
-    public String getPublicFullApplicationUrl() {
-        final String prefix = servletRequest.isSecure() ? "https://" : "http://";
-
-        final String fullApplicationPath = prefix
-                + InetAddress.getLoopbackAddress().getHostAddress()
-                + ":"
-                + servletRequest.getLocalPort()
-                + servletContext.getContextPath();
-        log.debug("Full application path: {}", fullApplicationPath);
-        return fullApplicationPath;
-    }
 }
