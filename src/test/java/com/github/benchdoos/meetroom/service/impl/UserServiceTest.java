09dc01e33279577c262818a5dd0ed462cd698c88
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-07-09 11:47:47 +0300

    meetroom-5: Added some tests

--- src/test/java/com/github/benchdoos/meetroom/service/impl/UserServiceTest.java
+++ src/test/java/com/github/benchdoos/meetroom/service/impl/UserServiceTest.java
@@ -1,25 +1,25 @@
 package com.github.benchdoos.meetroom.service.impl;
 
 import com.github.benchdoos.meetroom.abstracts.AbstractIntegrationCommonTest;
-import com.github.benchdoos.meetroom.config.TestMailSenderConfig;
 import com.github.benchdoos.meetroom.config.beans.SpringConfigurationInfoBean;
 import com.github.benchdoos.meetroom.domain.dto.CreateUserDto;
 import com.github.benchdoos.meetroom.domain.dto.UserPublicInfoDto;
+import com.github.benchdoos.meetroom.exceptions.EmailAlreadyExistsException;
+import com.github.benchdoos.meetroom.exceptions.UserAlreadyExistsException;
 import com.github.benchdoos.meetroom.repository.UserRepository;
 import com.github.benchdoos.meetroom.service.EmailService;
 import com.github.benchdoos.meetroom.service.UserService;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserServiceTest extends AbstractIntegrationCommonTest {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -34,23 +34,26 @@ public class UserServiceTest extends AbstractIntegrationCommonTest {
 
     @Override
     public void setUp() throws Exception {
+
         super.setUp();
         MockitoAnnotations.initMocks(this);
 
-        Mockito.when(springConfigurationInfoBean.getPublicFullApplicationUrl()).thenReturn("http://valid-service-url.com/");
+        Mockito.when(springConfigurationInfoBean.getPublicFullApplicationUrl())
+                .thenReturn("http://valid-service-url.com/");
     }
-
 
     @Test
     public void createNewUserMustCreateNewUser() {
-        final CreateUserDto createUserDto = CreateUserDto.builder()
-                .firstName("First name")
-                .lastName("Last name")
-                .password("qwertyuiop")
-                .confirmPassword("qwertyuiop")
-                .username("correctusername")
-                .email("correct-email@mail.com")
-                .build();
+
+        final CreateUserDto createUserDto =
+                CreateUserDto.builder()
+                        .firstName("First name")
+                        .lastName("Last name")
+                        .password("qwertyuiop")
+                        .confirmPassword("qwertyuiop")
+                        .username("correctusername")
+                        .email("correct-email@mail.com")
+                        .build();
         final UserPublicInfoDto user = userService.createUser(createUserDto);
 
         assertThat(user).isNotNull();
@@ -60,6 +63,36 @@ public class UserServiceTest extends AbstractIntegrationCommonTest {
         assertThat(user.getEmail()).isEqualTo(createUserDto.getEmail());
         assertThat(user.getEnabled()).isEqualTo(true);
         assertThat(user.getAvatar()).isNotNull();
+    }
+
+    @Test(expected = UserAlreadyExistsException.class)
+    public void createNewUserMustThrowUserAlreadyExistsException() {
+
+        final CreateUserDto alreadyCreatedUser = CreateUserDto.builder()
+                .firstName("First name")
+                .lastName("Last name")
+                .password("qwertyuiop")
+                .confirmPassword("qwertyuiop")
+                .username("user") //important
+                .email("correct-email@mail.com")
+                .build();
+        final UserPublicInfoDto user = userService.createUser(alreadyCreatedUser);
+    }
+
+    @Sql("/sql/UserServiceTest.sql")
+    @Test(expected = EmailAlreadyExistsException.class)
+    public void createNewUserMustThrowEmailAlreadyExistsException() {
+
+        final CreateUserDto alreadyCreatedUser = CreateUserDto.builder()
+                .firstName("First name")
+                .lastName("Last name")
+                .password("qwertyuiop")
+                .confirmPassword("qwertyuiop")
+                .username("correctusername")
+                .email("correct-test-user-email@mail.com") //important
+                .build();
+
+        final UserPublicInfoDto user = userService.createUser(alreadyCreatedUser);
     }
 
 }
