1d169159b200ee59eabe4577efaec5e91556d70e
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-07-12 15:33:15 +0300

    meetroom-8: Added user account activation sending

--- src/main/resources/static/js/requests/user/update-user-requests.js
+++ src/main/resources/static/js/requests/user/update-user-requests.js
@@ -165,6 +165,31 @@ function updateUserEmail(context, modalId, targetErrorObjectId, successDialog, u
 }
 
 /**
+ *
+ * @param context
+ * @param modalId
+ * @param targetErrorObjectId
+ * @param userId
+ */
+function sendUserActivationLink(context,  modalId, targetErrorObjectId, userId) {
+    let url = getApiV1Context(context) + '/manage/user/send-activation-request/' + userId;
+    console.log("sendUserActivationLink", url);
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        contentType: "application/json",
+        success: function (output, status, xhr) {
+            $('#' + modalId).modal('hide');
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            let responseJSON = XMLHttpRequest.responseJSON;
+            $('#' + targetErrorObjectId).html(responseJSON.message).show();
+        }
+    });
+}
+
+/**
  * Update modal dialog title and message
  *
  * @param successDialog dialog id
